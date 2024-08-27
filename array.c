4a1a8de3fa79367c9259e34ef8f223419fe58c2a
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-08-27 13:52:09 +0530

    lab3 completed here

---
+++ array.c
@@ -0,0 +1,114 @@
+// 1. write a program to creare 1D array
+
+// #include<stdio.h>
+// int main(){
+//     // int  arr[4] = {1,2,3,4};
+//     int arr[2];
+//     printf("enter 10 numbers\n");
+//     for(int i = 0;i<10;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     for(int i = 0;i<10;i++){
+//         printf("%d ",arr[i]);
+//     }
+// }
+
+//2. write a program to create 2D array
+// #include<stdio.h>
+// int main(){
+//     int arr[3][3];
+//     printf("enter elements of matrix: \n");
+//     for(int i = 0;i<3;i++){
+//         for(int j = 0;j<3;j++){
+//             printf("enter element [%d][%d]: ",i,j);
+//             scanf("%d",&arr[i][j]);
+//         }
+        
+//     }
+//     printf("matrix is: \n");
+//     for(int i = 0;i<3;i++){
+//         for(int j = 0;j<3;j++){
+//             printf("%d ",arr[i][j]);
+//         }
+//         printf("\n");S
+//     }
+// }
+
+
+// 3.WAP in C to insert an element at any specific location in an array.
+
+// #include<stdio.h>
+// int main(){
+//     int arr[5], ele, pos, i;
+//     printf("Enter element of array: \n");
+//     for(i = 0; i<5; i++){
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Enter element to insert: \n");
+//     scanf("%d", &ele);
+//     printf("Enter position to insert: \n");
+//     scanf("%d", &pos);
+//     for(i = 4; i>=pos-1; i--){
+//         arr[i+1] = arr[i];
+//     }
+//     arr[pos-1] = ele;
+//     for(i = 0; i<6; i++){
+//         printf("%d ", arr[i]);
+//     } 
+
+// }
+
+// 4. WAP in C to delete an element at any specific location in an array.
+// #include<stdio.h>
+
+// int main(){
+//     int arr[5], pos, i;
+
+//     printf("Enter element of array: \n");
+//     for(i = 0; i<5; i++){
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Enter position to delete: \n");     
+//     scanf("%d", &pos);
+//     for(i = pos; i<5; i++){
+//         arr[i] = arr[i+1];
+//     }
+//     for(i = 0; i<4; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// 5. WAP to perform transpse of 2d matrix
+
+#include<stdio.h>
+int main(){
+    int arr[3][3], transpose[3][3], i, j;
+    printf("Eneter Element of array: \n");
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+            transpose[i][j] = arr[j][i];
+        }
+    }
+
+    printf("Orginal matrix : \n");
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+             printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Transpose on matrix : \n");
+    for(i=0; i<3; i++){
+        for(j=0; j<3; j++){
+             printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ lab/lab4.c
@@ -0,0 +1,246 @@
+// wap for a menu driven program that enables user to implement linklist with all possible operations 
+// a) Insertion at the end
+// b) Insertion at the beginning
+// c) Insertion at the specific position
+// d) Deletion at end
+// e) Deletion at beginning
+// f) Deletion at specific postion
+// g) travesing 
+// h) counting the nodes
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function prototypes
+void insertAtEnd(struct Node** head, int data);
+void insertAtBeginning(struct Node** head, int data);
+void insertAtPosition(struct Node** head, int data, int position);
+void deleteAtEnd(struct Node** head);
+void deleteAtBeginning(struct Node** head);
+void deleteAtPosition(struct Node** head, int position);
+void traverse(struct Node* head);
+int countNodes(struct Node* head);
+
+// Main function
+int main() {
+    struct Node* head = NULL;
+    int choice, data, position;
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Insert at the end\n");
+        printf("2. Insert at the beginning\n");
+        printf("3. Insert at a specific position\n");
+        printf("4. Delete at the end\n");
+        printf("5. Delete at the beginning\n");
+        printf("6. Delete at a specific position\n");
+        printf("7. Traverse\n");
+        printf("8. Count nodes\n");
+        printf("9. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at the end: ");
+                scanf("%d", &data);
+                insertAtEnd(&head, data);
+                break;
+            case 2:
+                printf("Enter data to insert at the beginning: ");
+                scanf("%d", &data);
+                insertAtBeginning(&head, data);
+                break;
+            case 3:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                printf("Enter position to insert at: ");
+                scanf("%d", &position);
+                insertAtPosition(&head, data, position);
+                break;
+            case 4:
+                deleteAtEnd(&head);
+                break;
+            case 5:
+                deleteAtBeginning(&head);
+                break;
+            case 6:
+                printf("Enter position to delete: ");
+                scanf("%d", &position);
+                deleteAtPosition(&head, position);
+                break;
+            case 7:
+                traverse(head);
+                break;
+            case 8:
+                printf("Total nodes: %d\n", countNodes(head));
+                break;
+            case 9:
+                exit(0);
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+
+    return 0;
+}
+
+// Function to insert at the end of the list
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* temp = *head;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Function to insert at the beginning of the list
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert at a specific position
+void insertAtPosition(struct Node** head, int data, int position) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (position == 1) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    for (int i = 1; i < position - 1; i++) {
+        if (temp == NULL) {
+            printf("Position out of bounds\n");
+            free(newNode);
+            return;
+        }
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Position out of bounds\n");
+        free(newNode);
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+// Function to delete at the end of the list
+void deleteAtEnd(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node* temp = *head;
+    if (temp->next == NULL) {
+        free(temp);
+        *head = NULL;
+        return;
+    }
+
+    struct Node* prev = NULL;
+    while (temp->next != NULL) {
+        prev = temp;
+        temp = temp->next;
+    }
+    prev->next = NULL;
+    free(temp);
+}
+
+// Function to delete at the beginning of the list
+void deleteAtBeginning(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node* temp = *head;
+    *head = temp->next;
+    free(temp);
+}
+
+// Function to delete at a specific position
+void deleteAtPosition(struct Node** head, int position) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node* temp = *head;
+
+    if (position == 1) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    struct Node* prev = NULL;
+    for (int i = 1; i < position; i++) {
+        if (temp == NULL) {
+            printf("Position out of bounds\n");
+            return;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        printf("Position out of bounds\n");
+        return;
+    }
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+// Function to traverse the list
+void traverse(struct Node* head) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to count the number of nodes in the list
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
