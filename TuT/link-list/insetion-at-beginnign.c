65a8b1c3f13054bc5f04fe596ce221878219b559
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-13 17:27:01 +0530

    insertion at the end finished

--- TuT/link-list/insetion-at-beginnign.c
+++ TuT/link-list/insetion-at-beginnign.c
@@ -1,3 +1,31 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+
+// struct Node{
+//     int data;
+//     struct Node* next;
+// };
+
+// void insertionAtBeginning(int data, struct Node** head);
+
+
+// void main(){
+//    struct Node* head = malloc(sizeof(struct Node));
+//    head->data = 57;
+//    head->next = NULL;
+//    printf("%d\n", head->data);
+
+//    insertionAtBeginning(60, &head);
+
+// }
+
+// void insertionAtBeginning(int data, struct Node** head){
+//      struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+//     newNode->data = data;
+//     newNode->next = *head;
+
+//     printf("%d", newNode->data);
+// }
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,19 +34,30 @@ struct Node{
     struct Node* next;
 };
 
-void insertionAtBeginning(int data, struct Node** head);
+void insertionAtEnd(int data, struct Node** head);
 
 
 void main(){
    struct Node* head = malloc(sizeof(struct Node));
    head->data = 57;
    head->next = NULL;
-   
+
+   insertionAtEnd(60, &head);
 
 }
 
-void insertionAtBeginning(int data, struct Node** head){
-     struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
-    newNode->data = 57;
+void insertionAtEnd(int data, struct Node** head){
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* temp = *head;
+    newNode->data = data;
     newNode->next = *head;
+
+    if(head == NULL){
+      *head = newNode;
+   }else{
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+   }
 }
