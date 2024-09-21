92dc73d24061338debac0ad7a54a18782fd2a8fd
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-09-21 16:48:31 +0530

    small changes

--- lab/lab2.c
+++ lab/lab2.c
@@ -23,22 +23,22 @@ int main(){
 }
 
 //4. WAP in C to delete an element at any specific location in an array.
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
-    int arr[5], pos, i;
+// int main(){
+//     int arr[5], pos, i;
 
-    printf("Enter element of array: \n");
-    for(i = 0; i<5; i++){
-        scanf("%d", &arr[i]);
-    }
+//     printf("Enter element of array: \n");
+//     for(i = 0; i<5; i++){
+//         scanf("%d", &arr[i]);
+//     }
 
-    printf("Enter position to delete: \n");     
-    scanf("%d", &pos);
-    for(i = pos; i<5; i++){
-        arr[i] = arr[i+1];
-    }
-    for(i = 0; i<4; i++){
-        printf("%d ", arr[i]);
-    }
-}
+//     printf("Enter position to delete: \n");     
+//     scanf("%d", &pos);
+//     for(i = pos; i<5; i++){
+//         arr[i] = arr[i+1];
+//     }
+//     for(i = 0; i<4; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
---
+++ lab/tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// int main(){
+//     int arr[5], pos, i;
+
+//     printf("Enter element of array: \n");
+//     for(i = 0; i<5; i++){
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Enter position to delete: \n");     
+//     scanf("%d", &pos);
+//     for(i = pos; i<5; i++){
+//         arr[i] = arr[i+1];
+//     }
+//     for(i = 0; i<4; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
