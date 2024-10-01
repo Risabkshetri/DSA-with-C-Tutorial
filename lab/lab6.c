e52bae99ba6e30bb4ea60b536e73b19e25b46a81
RisabKshetri <155343200+risaw33@users.noreply.github.com>
2024-10-01 14:10:15 +0530

    lab 6 end here

---
+++ lab/lab6.c
@@ -0,0 +1,17 @@
+// calculate the factorial of number using recusion
+
+#include<stdio.h>
+
+int factorial(int n){
+    if(n == 0 || n == 1){
+     return 1;
+    };
+    return n * factorial(n-1);
+};
+
+
+int main(){
+    int num = 3;
+    printf("factorial of 3 = %d", factorial(num));
+    return 0;
+}
